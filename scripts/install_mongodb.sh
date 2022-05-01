0a840ea247362cf685d096a6e93fab5e7a810e4b
feenord <feenord40@gmail.com>
2022-05-01 17:57:38 +0300

    monitoring v1

---
+++ scripts/install_mongodb.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#sudo apt-get install wget -y
+#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update -y
+sudo apt install mongodb-server -y
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
+sudo systemctl status mongodb
