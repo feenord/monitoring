a4901c4d2c62250e78a51bba71aa4dbbd628bd67
feenord <feenord40@gmail.com>
2022-05-01 19:23:32 +0300

    monitoring v0.4

--- scripts/install_monitoring.sh
+++ scripts/install_monitoring.sh
@@ -10,8 +10,9 @@ git clone -b master https://github.com/feenord/monitoring.git
 sudo mkdir -p /etc/prometheus
 sudo mkdir -p /var/lib/prometheus
 wget https://github.com/prometheus/prometheus/releases/download/v2.31.0/prometheus-2.31.0.linux-amd64.tar.gz
-tar -xvf prometheus-2.31.3.linux-amd64.tar.gz
-cd prometheus-2.31.3.linux-amd64 
+tar -xvf prometheus-2.31.0.linux-amd64.tar.gz
+cp ./scripts/prometheus.service /etc/systemd/system/prometheus.service
+cd prometheus-2.31.0.linux-amd64 
 sudo mv prometheus promtool /usr/local/bin/
 sudo mv consoles/ console_libraries/ /etc/prometheus/
 sudo mv prometheus.yml /etc/prometheus/prometheus.yml
@@ -21,8 +22,6 @@ sudo groupadd --system prometheus
 sudo useradd -s /sbin/nologin --system -g prometheus prometheus
 sudo chown -R prometheus:prometheus /etc/prometheus/ /var/lib/prometheus/
 sudo chmod -R 775 /etc/prometheus/ /var/lib/prometheus/
-cp ./scripts/prometheus.service /etc/systemd/system/prometheus.service
 sudo systemctl start prometheus
 sudo systemctl enable prometheus
 sudo systemctl status prometheus
-echo -ok
